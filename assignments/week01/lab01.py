be161f5d2ed68e94668dea19cb0c82fd98578b47
Jeejo Jurn <jeejo.j@ku.th>
2025-07-08 12:15:41 +0700

    homework01

---
+++ assignments/week01/lab01.py
@@ -0,0 +1,13 @@
+print("=== Personal profile Creater ===")
+name = input("What is your name? :")
+age = input("How old are you? :")
+email = input("What your email :")
+phone = input("What is your phone number :")
+hobby = input("What is your favorite hobby :")
+
+print("== Your profile ==")
+print("Name :",name)
+print("Age:",age)
+print("Email :",email)
+print("Phone number :",phone)
+print("Favorite hobby :",hobby)
