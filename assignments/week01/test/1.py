57ac61d9b4e3a6395730e227b89d30417729e334
Jeejo Jurn <jeejo.j@ku.th>
2025-09-02 11:55:31 +0700

    gg

---
+++ assignments/week01/test/1.py
@@ -0,0 +1,7 @@
+unit_used = 350
+rate_per_unit = 4.5
+service_charge = 50
+
+electricity_cost = unit_used * rate_per_unit
+total = electricity_cost + service_charge
+print("used  =  unit",unit_used)
